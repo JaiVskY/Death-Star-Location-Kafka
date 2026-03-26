ca96980d1d0a7654ad12cd1e48e8f0ab0e5f05f4
JaiVskY <jaismailforpics@gmail.com>
2026-03-27 01:03:32 +0530

    producer creation and API tests

---
+++ producer.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import time
+from confluent_kafka import Producer
+
+# Kafka Configuration
+conf = {'bootstrap.servers': 'localhost:9092'}   #address of the container => PC
+producer = Producer(conf)
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Location Sent to {msg.topic()} [{msg.partition()}]")
+
+def fetch_iss_location():
+    try:
+        response = requests.get("http://open-notify.org/Open-Notify-API/ISS-Location-Now/")
+        data = response.json()
+        return {
+            "timestamp": data['timestamp'],
+            "latitude": data['iss_position']['latitude'],
+            "longitude": data['iss_position']['longitude']
+        }
+    except Exception as e:
+        print(f"Error fetching API: {e}")
+        return None
+
+print("Starting ISS Producer... Press Ctrl+C to stop.")
+
+try:
+    while True:
+        iss_data = fetch_iss_location()
+        if iss_data:
+            # Send data as a JSON string
+            producer.produce(
+                'iss-tracking', 
+                value=json.dumps(iss_data), 
+                callback=delivery_report
+            )
+            producer.flush() # Ensure it sends immediately
+        
+        time.sleep(5) # Wait 5 seconds before next update
+except KeyboardInterrupt:
+    print("Stopping Producer...")
